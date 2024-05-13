b290994eed9d89cd8e945166fb810d1051fca850
Максим <120325159+Lumas12@users.noreply.github.com>
2024-05-13 15:28:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,13 @@
+// Copyright 2022 UNN-IASR
+#include "Automata.h"
+#include <iostream>
 
+int main() {
+    Automata one;
+    one.on();
+    one.coin(30);
+    one.choice(2);
+    one.check();
+    one.cook();
+    one.finish();
+}
