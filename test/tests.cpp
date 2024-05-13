ef84683487a75e8c0ef79492d2e17ad7eb046c11
Максим <120325159+Lumas12@users.noreply.github.com>
2024-05-13 15:49:10 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,96 +1,89 @@
-// Copyright 2022 GHA Test Team
-
 #include <gtest/gtest.h>
 #include "Automata.h"
 
-TEST1(AutomataTest, InitialState) {
-    Automata automata;
-    EXPECT_EQ(automata.getState(), Automata::STATES::OFF);
-    EXPECT_EQ(automata.Cash, 0);
+class AutomataTest : public ::testing::Test {
+protected:
+    Automata one;
+};
+
+TEST_F(AutomataTest, InitialState_1) {
+    EXPECT_EQ(one.getState(), Automata::STATES::OFF);
+    EXPECT_EQ(one.Cash, 0);
 }
 
-TEST2(AutomataTest, TurnOnAndOff) {
-    Automata automata;
-    automata.on();
-    EXPECT_EQ(automata.getState(), Automata::STATES::WAIT);
-    automata.off();
-    EXPECT_EQ(automata.getState(), Automata::STATES::OFF);
+TEST_F(AutomataTest, TurnOnAndOff_2) {
+    one.on();
+    EXPECT_EQ(one.getState(), Automata::STATES::WAIT);
+    one.off();
+    EXPECT_EQ(one.getState(), Automata::STATES::OFF);
 }
 
-TEST3(AutomataTest, InsertCoin) {
-    Automata automata;
-    automata.on();
-    automata.coin(10);
-    EXPECT_EQ(automata.getState(), Automata::STATES::ACCEPT);
-    EXPECT_EQ(automata.Cash, 10);
+TEST_F(AutomataTest, InsertCoin_3) {
+    one.on();
+    one.coin(10);
+    EXPECT_EQ(one.getState(), Automata::STATES::ACCEPT);
+    EXPECT_EQ(one.Cash, 10);
 }
 
-TEST4(AutomataTest, DisplayMenu) {
-    Automata automata;
-    automata.on();
-    automata.getMenu();
+TEST_F(AutomataTest, DisplayMenu_4) {
+    one.on();
+    one.getMenu();
+    // Проверка, что функция вызвана без ошибок
     EXPECT_TRUE(true);
 }
 
-TEST5(AutomataTest, MakeChoice) {
-    Automata automata;
-    automata.on();
-    automata.coin(20);
-    automata.choice(0); 
-    automata.check();
-    EXPECT_EQ(automata.getState(), Automata::STATES::WAIT);
-    EXPECT_EQ(automata.Cash, 10); 
+TEST_F(AutomataTest, MakeChoice_5) {
+    one.on();
+    one.coin(20);
+    one.choice(0); // Выбор чая
+    one.check();
+    EXPECT_EQ(one.getState(), Automata::STATES::WAIT);
+    EXPECT_EQ(one.Cash, 10); // Остаток после покупки чая
 }
 
-TEST6(AutomataTest, CancelOperation) {
-    Automata automata;
-    automata.on();
-    automata.coin(20);
-    automata.choice(0); 
-    automata.cancel();
-    EXPECT_EQ(automata.getState(), Automata::STATES::WAIT);
-    EXPECT_EQ(automata.Cash, 20); 
+TEST_F(AutomataTest, CancelOperation_6) {
+    one.on();
+    one.coin(20);
+    one.choice(0); // Выбор чая
+    one.cancel();
+    EXPECT_EQ(one.getState(), Automata::STATES::WAIT);
+    EXPECT_EQ(one.Cash, 20); // Деньги возвращены после отмены
 }
 
-TEST7(AutomataTest, CookingProcess) {
-    Automata automata;
-    automata.on();
-    automata.coin(20);
-    automata.choice(0); 
-    automata.check();
-    automata.cook();
-    automata.finish();
-    EXPECT_EQ(automata.getState(), Automata::STATES::WAIT);
+TEST_F(AutomataTest, CookingProcess_7) {
+    one.on();
+    one.coin(20);
+    one.choice(0); // Выбор чая
+    one.check();
+    one.cook();
+    one.finish();
+    EXPECT_EQ(one.getState(), Automata::STATES::WAIT);
 }
 
-TEST8(AutomataTest, CheckInsufficientFunds) {
-    Automata automata;
-    automata.on();
-    automata.coin(10);
-    automata.choice(0); 
-    automata.check();
-    EXPECT_EQ(automata.getState(), Automata::STATES::WAIT);
-    EXPECT_EQ(automata.Cash, 10); 
+TEST_F(AutomataTest, CheckInsufficientFunds_8) {
+    one.on();
+    one.coin(10);
+    one.choice(0); // Выбор чая
+    one.check();
+    EXPECT_EQ(one.getState(), Automata::STATES::WAIT);
+    EXPECT_EQ(one.Cash, 10); // Проверка, что деньги не были списаны, так как недостаточно средств
 }
 
-TEST9(AutomataTest, CheckSufficientFunds) {
-    Automata automata;
-    automata.on();
-    automata.coin(20);
-    automata.choice(0); 
-    automata.check();
-    EXPECT_EQ(automata.getState(), Automata::STATES::WAIT);
-    EXPECT_EQ(automata.Cash, 10); 
+TEST_F(AutomataTest, CheckSufficientFunds_9) {
+    one.on();
+    one.coin(20);
+    one.choice(0); // Выбор чая
+    one.check();
+    EXPECT_EQ(one.getState(), Automata::STATES::WAIT);
+    EXPECT_EQ(one.Cash, 10); // Проверка, что деньги были списаны после успешной покупки
 }
 
-TEST10(AutomataTest, MultipleChoices) {
-    Automata automata;
-    automata.on();
-    automata.coin(20);
-    automata.choice(0); 
-    automata.choice(1); 
-    automata.check();
-    EXPECT_EQ(automata.getState(), Automata::STATES::WAIT);
-    EXPECT_EQ(automata.Cash, 10); 
+TEST_F(AutomataTest, MultipleChoices_10) {
+    one.on();
+    one.coin(20);
+    one.choice(0); // Выбор чая
+    one.choice(1); // Попытка выбрать кофе после чая
+    one.check();
+    EXPECT_EQ(one.getState(), Automata::STATES::WAIT);
+    EXPECT_EQ(one.Cash, 10); // Проверка, что деньги не были списаны, так как уже была сделана покупка
 }
-
