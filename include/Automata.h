fc3594e97f42d91b9479942a1dcd91ce4c2b5d0d
Максим <120325159+Lumas12@users.noreply.github.com>
2024-05-13 15:25:31 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,1 +1,40 @@
+// Copyright 2022 UNN-IASR
 
+#pragma once
+#include <iostream>
+#include <array>
+#include <string>
+#include <stdexcept>
+
+class Automata {
+ public:
+    Automata();
+
+ public:
+    void on();
+    void off();
+    void coin(const int);
+    void getMenu();
+    int getState();
+    void choice(const int);
+    void check();
+    void cancel();
+    void cook();
+    void finish();    
+
+ private:
+    enum STATES {
+        OFF,
+        WAIT,
+        ACCEPT,
+        CHECK,
+        COOK
+    };
+
+ private:
+    size_t Choice;
+    double Cash;
+    std::array<std::string, 10> Menu;
+    std::array<int, 10> Prices;
+    STATES State;
+};
